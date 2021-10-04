790bf57472db1a75448e669995629321fa7822b1
SaurabhChawla <s.saurabhtim@gmail.com>
2021-10-04 19:42:38 +0530

    Add the Support for collecting the Spark application Metrics

--- spark-radiant-core/src/main/scala/com/spark/radiant/core/SparkJobMetricsCollector.scala
+++ spark-radiant-core/src/main/scala/com/spark/radiant/core/SparkJobMetricsCollector.scala
@@ -17,11 +17,70 @@
 
 package com.spark.radiant.core
 
+import org.apache.spark.internal.Logging
+import org.apache.spark.scheduler._
+
+import scala.collection.mutable.HashMap
+
 /**
- *  Class of collecting the important metrics to Spark Driver Memory, Stage metrics, Task Metrics
+ *  SparkJobMetricsCollector is used for collecting the important metrics to
+ *  Spark Driver Memory, Stage metrics, Task Metrics. This is enabled by
+ *  using the configuration
+ *  --conf spark.extraListeners=com.spark.radiant.core.SparkJobMetricsCollector
  */
 
+class SparkJobMetricsCollector()
+  extends SparkListener with Logging {
 
-class SparkJobMetricsCollector {
-// TODO work on this feature
+  private val jobInfoMap: HashMap[Long, JobInfo] = HashMap.empty
+  private var startTime: Long = 0
+
+  // TODO work on this feature
+
+  override def onJobStart(jobStart: SparkListenerJobStart): Unit = {
+    val jobInfo = jobInfoMap.getOrElseUpdate(jobStart.jobId,
+      JobInfo(jobStart.jobId, jobStart.time))
+    jobInfoMap.put(jobStart.jobId, jobInfo)
+  }
+
+  override def onJobEnd(jobEnd: SparkListenerJobEnd): Unit = {
+    val jobInfo = jobInfoMap.getOrElseUpdate(jobEnd.jobId,
+      JobInfo(jobEnd.jobId, 0, jobEnd.time))
+    jobInfo.jobEnd = jobEnd.time
+    jobInfoMap.put(jobEnd.jobId, jobInfo)
+  }
+
+  override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted): Unit = {
+  }
+
+  override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
+  }
+
+  override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
+  }
+
+  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
+    startTime = applicationStart.time
+  }
+
+  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {
+    val endTime = applicationEnd.time - startTime
+    logInfo(s"Total Time taken by Application:: $endTime ms")
+    val jobTime = jobInfoMap.map(info => info._2.jobEnd - info._2.jobStart).sum
+    val timeSpendInDriver = endTime - jobTime
+    logInfo(s"Time spend in the Driver:  $timeSpendInDriver ms")
+    val percentDriverTime = (timeSpendInDriver*100)/endTime
+    if (percentDriverTime > 25) {
+      logInfo(s"Percentage of time spend in the Driver: $percentDriverTime." +
+        s" Try adding more parallelism to the Spark job for Optimal Performance")
+    }
+  }
+
+  private def addSkewTaskInfo(): Unit = {
+  }
+
+  private def getSkewTaskInfo(): Unit = {
+  }
 }
+
+case class JobInfo(jobId: Long = -1L, var jobStart: Long = 0L, var jobEnd: Long = 0L)
